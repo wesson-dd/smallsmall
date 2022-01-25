0f6117bc02e049752847c247f73895f4f3a5627d
wesson <work_wmd@163.com>
2022-01-26 01:52:16 +0800

    feat:窝窝头

--- src/main/java/com/small/small/pojo/DynamicMapperParams.java
+++ src/main/java/com/small/small/pojo/DynamicMapperParams.java
@@ -1,20 +1,22 @@
 package com.small.small.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 import java.util.List;
 
 /**
- * description:
- * <p>
  * Create By wesson 2020-09-20 20:37
  *
  * @author wesson
  */
 @NoArgsConstructor
 @Data
+@AllArgsConstructor
+@Accessors(chain = true)
 public class DynamicMapperParams implements Serializable {
     private static final long serialVersionUID = -2029047708406518991L;
 
