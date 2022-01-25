aac7b471c837d6859d9bdbb1c935462720255bdb
wesson <work_wmd@163.com>
2022-01-26 03:08:51 +0800

    feat:new day

---
+++ src/main/java/com/small/leetcode/Action5.java
@@ -0,0 +1,13 @@
+package com.small.leetcode;
+
+/**
+ * @author wesson
+ * @date 2020/8/14   00:36
+ */
+public class Action5 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/com/small/util/SynchronizedUtil.java
@@ -0,0 +1,30 @@
+package com.small.util;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 同步锁工具类 注入使用
+ *
+ * @author wesson
+ * Create at 2022/1/26 00:54 周三
+ */
+public class SynchronizedUtil {
+    public SynchronizedUtil() {
+    }
+
+    Map<String, Object> mutexCache = new ConcurrentHashMap<>();
+
+    public void exec(String key, Runnable statement) {
+        Object mutexKey = mutexCache.computeIfAbsent(key, v -> new Object());
+        synchronized (mutexKey) {
+            try {
+                statement.run();
+            } finally {
+                mutexCache.remove(key);
+            }
+        }
+
+    }
+
+}
