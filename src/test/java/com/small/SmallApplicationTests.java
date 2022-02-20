08ca678291e317e568230dc867a6e2e85e106827
wesson <work_wmd@163.com>
2022-02-20 20:44:55 +0800

    feat:2022年02月20日20:44:51

--- src/test/java/com/small/SmallApplicationTests.java
+++ src/test/java/com/small/SmallApplicationTests.java
@@ -1,13 +1,18 @@
 package com.small;
 
+import com.small.service.TestService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SmallApplicationTests {
+    @Autowired
+    TestService testService;
 
     @Test
     void contextLoads() {
+        System.out.println(testService.getList());
     }
 
 }
