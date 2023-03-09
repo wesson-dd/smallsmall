c43f255a59c5294590f65686344871083ac7fd2f
wesson <work_wmd@163.com>
2023-03-09 23:37:44 +0800

    feature：StreamEx

--- src/main/java/com/small/pojo/Test.java
+++ src/main/java/com/small/pojo/Test.java
@@ -2,6 +2,8 @@ package com.small.pojo;
 
 import cn.hutool.core.annotation.Alias;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -11,6 +13,8 @@ import java.math.BigDecimal;
  * Create at 2022/3/24 10:48 周四
  */
 @Data
+@Accessors(chain = true)
+@EqualsAndHashCode
 public class Test implements Serializable {
 
     private static final long serialVersionUID = -4725420540624500836L;
@@ -24,5 +28,6 @@ public class Test implements Serializable {
     private Boolean aBoolean = true;
     @Alias("天")
     private Long aLong = 2L;
+
     private BigDecimal bigDecimal = new BigDecimal("0");
 }
--- src/main/java/com/small/util/StreamExDemo.java
+++ src/main/java/com/small/util/StreamExDemo.java
@@ -1,8 +1,12 @@
 package com.small.util;
 
+import cn.hutool.core.lang.Console;
+import com.google.common.collect.Lists;
+import com.small.pojo.Test;
 import one.util.streamex.StreamEx;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author wesson
@@ -10,9 +14,23 @@ import java.util.List;
  */
 public class StreamExDemo {
     public static void main(String[] args) {
-        final List<Integer> integers = StreamEx.of(1, 2, 3, 4, 5).toList();
-        System.out.println(integers);
+        List<Test> one = StreamEx.of(new Test().setName("A-"),
+                new Test().setName("B-")).toList();
+        Console.log(one);
 
-        System.out.println(StreamEx.of(1, 2, 3, 45).without(3).toList());
+        List<Test> objects = Lists.newArrayList();
+        objects.addAll(one);
+        Console.log(objects);
+
+        List<Test> collect = one.stream().map(w -> {
+
+            final Test test = new Test();
+            test.setName(w.getName().replace("-", ""));
+            return test;
+        }).collect(Collectors.toList());
+
+        Console.log(one);
+        Console.log(objects);
+        Console.log(collect);
     }
 }
