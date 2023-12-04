42b088d08ec165d9fd136679eb0e3f9f6e32db13
wesson <work_wmd@163.com>
2023-12-05 00:58:26 +0800

    feature：@Indexed

--- src/test/java/com/small/CollectionTest.java
+++ src/test/java/com/small/CollectionTest.java
@@ -1,10 +1,14 @@
 package com.small;
 
+import cn.hutool.core.collection.CollStreamUtil;
 import cn.hutool.core.lang.Console;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import com.small.pojo.TestPojo;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 
@@ -39,6 +43,29 @@ public class CollectionTest {
         }));
 
         Console.log(map2);
+
+
+    }
+
+    @Test
+    public void MapTest2() {
+        final TestPojo testPojo = new TestPojo();
+        testPojo.setId(1);
+        testPojo.setName("test1");
+        final TestPojo testPojo2 = new TestPojo();
+        testPojo2.setId(2);
+        testPojo2.setName("test2");
+        final TestPojo testPojo3 = new TestPojo();
+        testPojo3.setId(2);
+        testPojo3.setName("test3");
+        final TestPojo testPojo4 = new TestPojo();
+        testPojo4.setId(4);
+        testPojo4.setName("test4");
+
+        final ArrayList<TestPojo> testPojos = Lists.newArrayList(testPojo, testPojo2, testPojo3, testPojo4);
+
+        final Map<Integer, TestPojo> identityMap = CollStreamUtil.toIdentityMap(testPojos, TestPojo::getId);
+        System.out.println(identityMap);
 
 
     }
